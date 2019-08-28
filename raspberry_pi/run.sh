e7d8bc38f46c02b5914e22468d38f4814794ffa9
PatrickHallek <patrickhallek1998@gmail.com>
2019-08-28 12:01:59 +0200

    Change run.sh paths

--- raspberry_pi/run.sh
+++ raspberry_pi/run.sh
@@ -4,8 +4,8 @@ trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
 echo "Starting all scripts script..."
-python ultrasonic.py &
-python localization.py &
-npm start  --prefix ../backend/
+python ./scripts/ultrasonic.py &
+python ./scripts/localization.py &
+npm start  --prefix ./backend/
 
 wait
